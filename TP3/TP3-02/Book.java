531e3dc92b76c1a7f89361577181830644ed0067
MarcoKouyate <marco.kouyate@yahoo.fr>
2019-03-01 13:44:16 +0100

    TP3 - Exercice 02 - ArrayList

--- TP3/TP3-02/Book.java
+++ TP3/TP3-02/Book.java
@@ -3,12 +3,12 @@ public class Book {
         private String title = "<no title>";
         private String author = "<no author>";
 
-	        public Book(String title, String author){
-	        	this.title = title;
+	      public Book(String title, String author){
+	         	this.title = title;
 	        	this.author = author;
 	        }
 			
-			public Book(String title){
+			  public Book(String title){
 	        	this.title = title;
 	        }
 			
@@ -22,4 +22,23 @@ public class Book {
       		{
         		return author;
       		}  
+
+          
+          //redefinit la fonction testant l'égalité de deux livres afin de comparer leurs champs
+          @Override public boolean equals(Object obj) {
+          if (this == obj){
+            return true;
+          }
+
+          if (obj==null){
+            return false;
+          }
+   
+          if (getClass() != obj.getClass()) {
+              return false;
+          }
+          
+          Book other = (Book) obj;
+          return (this.title.equals(other.title) && this.author.equals(other.author));
+        }
 }
--- TP3/TP3-02/Main.java
+++ TP3/TP3-02/Main.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
+
 public class Main {
-	public static void main (String[] args){
-	   var b1 = new Book("Da Java Code", "Duke Brown");
+     public static void main(String[] args){
+       var b1 = new Book("Da Java Code", "Duke Brown");
        var b2 = b1;
        var b3 = new Book("Da Java Code", "Duke Brown");
 
-       System.out.println(b1 == b2);
-       System.out.println(b1 == b3);
-	}
+       var list = new ArrayList();
+       list.add(b1);
+       System.out.println(list.indexOf(b2));
+       System.out.println(list.indexOf(b3));
+     }
 }
