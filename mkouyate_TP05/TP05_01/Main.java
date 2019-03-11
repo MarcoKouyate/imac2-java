fb36a67a3ef3f7c0b7610387122aff22373839b1
MarcoKouyate <marco.kouyate@yahoo.fr>
2019-03-11 10:03:25 +0100

    TP05-Exercice 01 - Done

--- mkouyate_TP05/TP05_01/Main.java
+++ mkouyate_TP05/TP05_01/Main.java
@@ -3,8 +3,11 @@ import fr.umlv.fight.*;
 public class Main {
      public static void main(String[] args){
 
-   	 var bob = new Robot("bob");
-     System.out.println(bob);  // affiche "Robot bob"
+   	 var d2r2 = new Robot("D2R2");
+   	 var data = new Robot("data");
+   	 var arena = new Arena();
+
+     System.out.println(arena.fight(d2r2,data) + " a remporté le combat");  // affiche "Robot bob"
     
      }
 }
--- mkouyate_TP05/TP05_01/fr/umlv/fight/Robot.java
+++ mkouyate_TP05/TP05_01/fr/umlv/fight/Robot.java
@@ -10,6 +10,23 @@ public class Robot {
   public Robot (String name) {
   	this.name=name;
   }
+
+  public void fire(Robot enemy) {
+  	enemy.hit();
+  	 System.out.println("Robot " + enemy.getName() + " a été touché par " + name +" !");
+  }
+
+  public void hit(){
+  	life = life-2;
+  }
+
+  public String getName(){
+  	return name;
+  }
+
+  public boolean isDead (){
+  	return (life <= 0 ) ? true : false;
+  }
   
   @Override public String toString(){
      return "Robot " + name;
