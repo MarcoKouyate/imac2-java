2b7ab991412e3a287d58b44067ae07fea98134d6
MarcoKouyate <marco.kouyate@yahoo.fr>
2019-03-11 10:31:26 +0100

    TP05 - Exercice 02 - End of class

---
+++ mkouyate_TP05/TP05_02/fr/umlv/fight/Fighter.java
@@ -0,0 +1,15 @@
+package fr.umlv.fight;
+
+import java.util.Objects;
+
+
+public class Fighter extends Robot {
+
+  public Fighter(String name){
+    super(name);
+  }
+  
+  @Override public String toString(){
+     return "Fighter " + name;
+  }  
+}
